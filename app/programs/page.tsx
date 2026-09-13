413c48427346b8b1371231b18cd20cddd3888fb8
loveedify <love.edify317@outlook.com>
2026-09-13 19:12:52 -0400

    Update page.tsx
    updated images

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -30,7 +30,7 @@ const programs = [
       'Clean, safe, and developmentally appropriate nursery',
     ],
     image:
-      'https://images.unsplash.com/photo-1544126592-807ade215a0b?w=600&q=80&fit=crop',
+      'https://static.wixstatic.com/media/c73eb8_5ca452edc08e4e4f9255e2b03ce10cea~mv2.jpg',
     color: 'bg-golden-500',
   },
   {
@@ -51,7 +51,7 @@ const programs = [
       'Bible stories and simple faith-centered lessons',
     ],
     image:
-      'https://images.unsplash.com/photo-1609834008399-c00e10728e7b?q=80&w=1170&auto=format&fit=crop',
+      'https://static.wixstatic.com/media/c73eb8_1d1a18a1437f4194882b5bea3a680e3f~mv2.jpg',
     color: 'bg-sage-400',
   },
   {
@@ -72,7 +72,7 @@ const programs = [
       'Faith-based character development and Bible lessons',
     ],
     image:
-      'https://images.unsplash.com/photo-1516627145497-ae6968895b74?q=80&w=1170&auto=format&fit=crop',
+      'https://static.wixstatic.com/media/c73eb8_b0b7ed375b9d423f92b8cd16ddf9a47f~mv2.jpg',
     color: 'bg-forest-500',
   },
   {
@@ -93,7 +93,7 @@ const programs = [
       'Themed learning units exploring community and nature',
     ],
     image:
-      'https://images.unsplash.com/photo-1553641118-066cde5e7047?q=80&w=1170&auto=format&fit=crop',
+      'https://static.wixstatic.com/media/c73eb8_77d24bf886b54a358661d682fb600642~mv2.jpg',
     color: 'bg-golden-500',
   },
   {
@@ -114,7 +114,7 @@ const programs = [
       'Kindergarten readiness assessments and progress reports',
     ],
     image:
-      'https://images.unsplash.com/photo-1559918095-0e91f93a8dd8?q=80&w=1170&auto=format&fit=crop',
+      'https://static.wixstatic.com/media/c73eb8_29535b66548947d59a4d77bf7e197cb7~mv2.jpg',
     color: 'bg-sage-400',
   },
   
