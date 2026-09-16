01c2d10f209ada30dca017e3b0d38fad292f72b7
loveedify <love.edify317@outlook.com>
2026-09-16 17:44:03 -0400

    Update page.tsx
    update images 2-3, 3-4

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -72,7 +72,7 @@ const programs = [
       'Faith-based character development and Bible lessons',
     ],
     image:
-      'https://static.wixstatic.com/media/c73eb8_b0b7ed375b9d423f92b8cd16ddf9a47f~mv2.jpg',
+      'https://static.wixstatic.com/media/c73eb8_202fc24f71ad4ed79aa52a7486bd36fe~mv2.jpg',
     color: 'bg-forest-500',
   },
   {
@@ -93,7 +93,7 @@ const programs = [
       'Themed learning units exploring community and nature',
     ],
     image:
-      'https://static.wixstatic.com/media/c73eb8_77d24bf886b54a358661d682fb600642~mv2.jpg',
+      'https://static.wixstatic.com/media/c73eb8_77e9c60856234b84a8a010b00a5909a9~mv2.jpg',
     color: 'bg-golden-500',
   },
   {
